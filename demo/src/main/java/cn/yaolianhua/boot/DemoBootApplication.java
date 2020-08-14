40ebaa21613e3a4d0de927bf6f112d7f05ba97c8
yaolianhua <yaolianhua789@gmail.com>
2020-08-14 22:19:39 +0800

    test demo add

---
+++ demo/src/main/java/cn/yaolianhua/boot/DemoBootApplication.java
@@ -0,0 +1,15 @@
+package cn.yaolianhua.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yaolianhua789@gmail.com
+ * @date 2020-08-14 21:44
+ **/
+@SpringBootApplication
+public class DemoBootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBootApplication.class,args);
+	}
+}
